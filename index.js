a1d96b06241c50ace4d9239d832845e219649483
MohammadUvesh-Ghanchi <MohammadUveshMustakbhai.Ghanchi@edu.sait.ca>
2025-09-09 16:38:16 -0600

    Add 'defer' attribute to script tag and update character list rendering in index.js; change background color and center text in main.css

--- index.js
+++ index.js
@@ -17,13 +17,11 @@ const users = [
 // broken test data for exercise 6
 
 // 1. Print out the names of each character in the console, then render them in the HTML list with id "names-list"
-console.log("All character names:");
+console.log("All Characters:");
+users.forEach(user => console.log(user.name));
 const namesList = document.getElementById("names-list");
-users.forEach((user) => {
-  console.log(user.name);
-  const listItem = document.createElement("li");
-  listItem.textContent = user.name;
-  namesList.appendChild(listItem);
+users.forEach(user => {
+  namesList.innerHTML = users.map(u => `<li>${u.name}</li>`).join("");
 });
 // 2. Print out the names of characters whose age is less than 40 in the console, then render them in the HTML list with id "young-characters-list"
 console.log("Characters under 40:");
@@ -31,7 +29,11 @@ const youngCharactersList = document.getElementById("young-characters-list");
 users.forEach((user) => {
   if (user.age < 40) {
     console.log(user.name);
-  }});
+    youngCharactersList.innerHTML = users
+      .filter((u) => u.age < 40)
+      .map((u) => `<li>${u.name}</li>`)
+      .join("");
+  } });
 // 3. Create a reusable function that takes any array and uses logic to render a list of character names in the HTML. Use this function to populate the list with id "function-list"
 
 // 4. Create a function that takes an array and an age threshold parameter. The function should only display characters whose age is below the given number. Render results in the list with id "age-filter-list"
